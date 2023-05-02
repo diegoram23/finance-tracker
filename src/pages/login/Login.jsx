d92f477e57749b771dada2f399b15ab1c27cdc73
diegoram23 <diego.pta.oc@gmail.com>
2023-05-02 09:29:13 -0700

    pages directory created

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+import styles from './Login.module.css'
+
+const Login = () => {
+    return ( 
+        <>
+        Login Page
+        </>
+     );
+}
+ 
+export default Login;
---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,11 @@
+import styles from './Signup.module.css'
+
+const Signup = () => {
+    return ( 
+        <>
+        Signup page
+        </>
+     );
+}
+ 
+export default Signup;
